9454dd1debce232ceacc8895ab3b27413feffa19
Sean Flaherty <sean.flaherty@gmail.com>
2015-01-09 12:29:02 -0500

    Initial commit to new project on git hub.

---
+++ templates/_header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta name="mobile-web-app-capable" content="yes">
+
+  <title>PicticiousPro</title>
+  <script src="<?=$www_root?>components/Boxer/jquery.fs.boxer.min.js"></script>
+  <script src="<?=$www_root?>components/jquery/dist/jquery.min.js"></script>
+  <script src="js/main.js"></script>
+  <link href="<?=$www_root?>components/modernizr/modernizr.js"></script>
+
+  <link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300|Roboto:400italic,700italic,700,400' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" type='text/css' href="css/site.min.css">
+  <!-- <link rel="stylesheet" href="css/site.min.css"> -->
+  <!-- <link rel="stylesheet" href="components/normalize-css/normalize.css">
+  <link rel="stylesheet" href="components/Gridlock/fs.gridlock.min.css">
+  <link rel="stylesheet" href="css/main.css"> -->
+
+</head>
+
+<body class="gridlock">
+  <header class="row logo_container">
+    <div class="logo_wrapper row">
+      <div class="mobile-full tablet-full desktop-full max-11 centered">
+        <h1 class="logo">PicticiousPro</h1>
+      </div>
+    </div>
+  </header>
---
+++ templates/home.php
@@ -0,0 +1,88 @@
+<section class="intro row">
+    <div class="row intro_row">
+      <div class="mobile-full tablet-4 desktop-6 max-5 max-push-1">
+        <h2 class="intro-title">Nice Shot</h2>
+        <p class="intro_text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.
+        </p>
+        <a href="#apps" class="button">Download Now</a>
+      </div>
+    </div>
+  </section>
+
+  <section class="features row">
+    <div class="desktop-4">
+      <h2 class="features-title tag">Tag It All</h2>
+      <p class="features-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit aliquet felis, quis.
+        <p>
+    </div>
+    <div class="desktop-4">
+      <h2 class="features-title cloud">Cloud Sync</h2>
+      <p class="features-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        <p>
+    </div>
+    <div class="desktop-4">
+      <h2 class="features-title photo">Photo Map</h2>
+      <p class="features-text">Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Ligula porta felis.
+      </p>
+    </div>
+  </section>
+
+  <section class="gallery row">
+    <div class="desktop-4">
+      <h3 class="gallery-title">Capture Life</h3>
+      <p class="gallery-text">Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.
+      </p>
+    </div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+		    <a href= "images/photos/01.jpg" class ="lightbox">
+                <img src="images/photos/thumb_01.jpg" alt="">
+            </a>
+    </figure></div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+        <a href= "images/photos/02.jpg" class ="lightbox">
+            <img src="images/photos/thumb_02.jpg" alt="">
+        </a>
+    </figure></div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+        <a href= "images/photos/03.jpg" class ="lightbox">
+            <img src="images/photos/thumb_03.jpg" alt="">
+        </a>
+    </figure></div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+        <a href= "images/photos/04.jpg" class ="lightbox">
+            <img src="images/photos/thumb_04.jpg" alt="">
+        </a>
+    </figure></div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+        <a href= "images/photos/05.jpg" class ="lightbox">
+            <img src="images/photos/thumb_05.jpg" alt="">
+        </a>
+    </figure></div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+        <a href= "images/photos/06.jpg" class ="lightbox">
+            <img src="images/photos/thumb_06.jpg" alt="">
+        </a>
+    </figure></div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+        <a href= "images/photos/07.jpg" class ="lightbox">
+            <img src="images/photos/thumb_07.jpg" alt="">
+        </a>
+    </figure></div>
+    <div><figure class="gallery-photo mobile-half tablet-1 desktop-2">
+        <a href= "images/photos/08.jpg" class ="lightbox">
+            <img src="images/photos/thumb_08.jpg" alt="">
+        </a>
+    </figure></div>
+  </section>
+
+  <section class="download row">
+    <div>
+      <h4 class="download-title">Download Now</h4>
+      <p class="download-text">Nullam id dolor id nibh ultricies vehicula deposit quid pro quo hic escorol.</p>
+      <ul>
+        <li class="apps_list_item"></li>
+        <li class="apps_list_item"></li>
+        <li class="apps_list_item"></li>
+      </ul>
+    </div>
+  </section>
